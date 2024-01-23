08049529ec4be8f3fd366fd45d647bba54811788
Jylant <125195997+Jylant@users.noreply.github.com>
2024-01-23 11:59:58 +0000

    Deploying to gh-pages from @ Jylant/portfolioPage@dc8fc2256e301e32e935d573f2d447c137c066f7 🚀

--- src/components/atoms/lang.jsx
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-
-export const languageAtom = atom({
-    key: 'language',
-    default: 'English',
-    choices: ['English', 'Finnish'],
-});
--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import { render } from 'preact'
-import { App } from './app.jsx' // Correct import statement
-import './styles/index.scss'
-
-render(<App />, document.getElementById('app'))
